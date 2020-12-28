a6292cae54bae8528fa0815f01b6b6cadb9a4827
Catarina <up201806610@fe.up.pt>
2020-12-28 13:43:56 +0000

    Display working, get empty line around square is a work in progress

--- src/display.pl
+++ src/display.pl
@@ -1,74 +1,39 @@
-/*  L1: lista com o numero de cada linha
-    L2: lista com o numero de cada coluna.
-intialSquare(L1, L2, Lines, Columns):-
-    writeLine, nl,
-    writeColumn(L),
-    writeLineNumber(Line1),
-    ....
-    write('  '),writeColumns(Columns).
+:- use_module(library(lists)).
+
+displayName:-
+    write("------ S Q U A R E ------").
+
+writeRow(Number,Number,Row):- 
+    nth1(Number,Row,Elem),
+    write(Elem),
+    write(' |').
+
+writeRow(0,Number,Row):-write('| '),writeRow(1,Number,Row).
+
+writeRow(N,Number,Row):-
+    nth1(N,Row,Elem),
+    write(Elem),
+    write(' | '),
+    NewN is N +1,
+    writeRow(NewN,Number,Row).
 
 
-    Columns: numeros que aparecem em baixo
-writeColumns([]).
-writeColumns([C|R]):-
-    write(C),write('   '),
-    writeColumns(R).
+writeLine(0):-write('-').
+writeLine(Number):-
+    write('----'),
+    NewNumber is Number - 1,
+    writeLine(NewNumber).
 
 
-
-    L: lista de 0 e 1 em que o quadrado fica pintado ou não
-writeColumn(L)
+displayMatrix(Matrix,Dimension,Dimension):-
+    writeLine(Dimension),nl,
+    nth1(Dimension,Matrix,Row),
+    writeRow(0,Dimension,Row),nl,
+    writeLine(Dimension).
     
-    0:quadrado não pintado
- writeSquare(0):-
-    write('| 0 ').
-
-    1: quadrado pintado
- writeSquare(1):-
-    write('| 1 ').
-
-    */
-
-
-
-
-
-/*Tentar descobrir o padrão*/
-initialSquare:-
-    writeLine, nl , writeColumn, writeLineNumber(2), nl, 
-    writeLine, nl , writeColumn, writeLineNumber(2), nl, 
-    writeLine, nl , writeColumn, writeLineNumber(2), nl, 
-    writeLine, nl , writeColumn, writeLineNumber(2), nl, 
-    writeLine, nl , writeColumn, writeLineNumber(3), nl, 
-    writeLine, nl , writeColumn, writeLineNumber(2), nl, 
-    writeLine, nl , writeColumn, writeLineNumber(2), nl, 
-    writeLine, nl , writeColumn, writeLineNumber(5), nl, 
-    writeLine, nl , writeColumn, writeLineNumber(3), nl, 
-    writeLine, nl , writeColumn, writeLineNumber(4), nl, writeLine,nl,
-    write('  '),writeColumnNumber(4), write('   '),
-    writeColumnNumber(5), write('   '),
-    writeColumnNumber(4), write('   '),
-    writeColumnNumber(4), write('   '),
-    writeColumnNumber(0), write('   '),
-    writeColumnNumber(0), write('   '),
-    writeColumnNumber(0), write('   '),
-    writeColumnNumber(4), write('   '),
-    writeColumnNumber(3), write('   '),
-    writeColumnNumber(3).
-    
-
-writeLine:-
-    write('-----------------------------------------').
-
-writeColumn:-
-    write('| 1 |   |   |   |   |   |   |   |   |   | ').
-
-/*numero que aparece em cada linha*/
-writeLineNumber(N):-
-    write(N).
-
-writeColumnNumber(N):-
-    write(N).
-
-initial:-
-    initialSquare.
+displayMatrix(Matrix,Dimension,Number):-
+    writeLine(Dimension),nl,
+    nth1(Number,Matrix,Row),
+    writeRow(0,Dimension,Row),nl,
+    NewNumber is Number + 1,
+    displayMatrix(Matrix,Dimension,NewNumber).
--- src/game.pl
+++ src/game.pl
@@ -1,7 +1,9 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
+:- include('display.pl').
 
 square :-
+    displayName,
     %4 x 4
     % Row restrictions
     RowR = [2, 2, 2, 2, 3, 2, 2, 5, 3, 4],
@@ -16,34 +18,26 @@ square :-
 
      1   1   1   1 -> ColumnR
     */
+    
+    /*
+        write("Dimension: "),
+        read(Number),
+    */
 
     Number is 10,
 
     buildList(Number, Number, [], Result),
-    /*domain(Result, 0, 1),
 
-    element(1, ResultRow, Elem),
-    Elem #= 1,
-    /*
-    element(1, Result, Row),
-    element(1, Row, Elem),
-    */
-    %element(1, Result, ResultRow),
-
+    %Restrictions
     apply_row_restrictions(Number, Result, RowR),
-    %trace,
     apply_column_restrictions(Number, Number, Result, ColumnR),
-    %notrace,
-    /*
-    nth1(1, Result, ResultRow),
-    element(1, ResultRow, Elem),
-    Elem #= 1,
-    */
+    apply_square_restrictions(Result),
 
     labeling_all(Number, Result),
 
     nl, write('Result:'), nl,
-    write(Result).
+    %write(Result)
+    displayMatrix(Result,Number,1).
 
 
 labeling_all(1, Matrix) :-
@@ -55,7 +49,7 @@ labeling_all(Number, Matrix) :-
     nth1(Number, Matrix, Row),
     labeling([], Row).
 
-
+%---------Restrictions---------------------
 apply_row_restrictions(1, Result, RowR) :-
     element(1, RowR, Elem),
     nth1(1, Result, ResultRow),
@@ -67,18 +61,6 @@ apply_row_restrictions(Number, Result, RowR) :-
     element(Number, RowR, Elem),
     nth1(Number, Result, ResultRow),
     sum(ResultRow, #=, Elem).
-
-
-get_column(1, NumberColumn, Matrix, List) :- 
-    nth1(1, Matrix, Row),
-    element(NumberColumn, Row, Elem),
-    append([], [Elem], List).
-get_column(NumberRow, NumberColumn, Matrix, List) :-
-    NewNumberRow is NumberRow - 1,
-    get_column(NewNumberRow, NumberColumn, Matrix, NewList),
-    nth1(NumberRow, Matrix, Row),
-    element(NumberColumn, Row, Elem),
-    append(NewList, [Elem], List).
 
 
 apply_column_restrictions(NumberRow, 1, Matrix, ColumnR) :-
@@ -93,9 +75,92 @@ apply_column_restrictions(NumberRow, NumberColumn, Matrix, ColumnR) :-
     get_column(NumberRow, NumberColumn, Matrix, ResultColumn),
     sum(ResultColumn, #=, Elem).
 
-    
+
+
+build_index_lists([H | _] , Result) :-
+
+
+build_index_lists(List, Result) :-
+
+
+get_extremes(List, ElemStart-ElemEnd) :- %List - Lista de listas com os indices adjacentes
+    length(List, LengthList),
+    nth1(1, List, ElemStart),
+    nth1(LengthList, List, ElemEnd).
+
+
+
+get_row_aux(Matrix, StartColumn, EndColumn, Row) :- %um dos casos base
+    StartColumn == EndColumn,
+    append([], [StartColumn], Row).
+get_row_aux(Matrix, StartColumn, EndColumn, Row) :-
+    StartColumn > 0,
+    NewStartColumn is StartColumn + 1,
+    get_row_aux(Matrix, NewStartColumn, EndColumn, NewRow),
+    append(NewRow, [StartColumn], Row).
+get_row_aux(Matrix, StartColumn, EndColumn, Row) :- %Se a start column estiver colada a uma parede
+    CurrentStartColumn is StartRow + 1,
+    NewStartColumn is CurrentStartColumn + 1,
+    get_row_aux(Matrix, NewStartColumn, EndColumn, NewRow),
+    append(NewRow, [CurrentStartColumn], Row).
+
+
+
+get_row(Matrix, [StartRow, StartColumn]-[StartRow, EndColumn], Row) :-
+    nth1(StartRow, Matrix, RowList),
+    NewStartColumn is StartColumn - 1,
+    NewEndColumn is EndColumn + 1,
+    get_row_aux(Matrix, NewStartColumn, NewEndColumn, Row),
+
+
+
+
+get_around(Matrix, [StartRow, StartColumn]-[EndRow, EndColumn], Result) :-
+    UpperRow is StartRow - 1,
+    get_row(Matrix, [UpperRow, StartColumn]-[UpperRow, EndColumn], UpperRow), %get upper row
+    get_row(Matrix, [EndRow, StartColumn]-[EndRow, EndColumn], UpperRow), %get lower row
+    get_around_aux(Matrix, [StartRow, StartColumn]-[EndRow, EndColumn], Result)
+    nth1()
+
+
+get_square_around(Matrix, List, Result) :- %List - Lista de listas com os indices adjacentes
+    get_extremes(List, Extremes),
+    get_around(Matrix, Extremes, Result).
 
     
+make_empty()
+
+
+square_restrictions(Matrix, List) :-
+    %get_square_around(Result)
+    %checks_filled(Result) result é o que está À volta
+    square_restrictions(Matrix, ListaComSquareEoqUEEstaAVolta).
+square_restrictions(Matrix, List) :-
+square_restrictions(Matrix, List) :-
+square_restrictions(Matrix, List) :-
+square_restrictions(Matrix, List) :-
+
+
+
+
+apply_square_restrictions(Matrix):-
+    % for each row
+        %for each cell
+        %square_restrictions(Matrix, [cell])
+
+
+%--------------------------------------------------------    
+
+get_column(1, NumberColumn, Matrix, List) :- 
+    nth1(1, Matrix, Row),
+    element(NumberColumn, Row, Elem),
+    append([], [Elem], List).
+get_column(NumberRow, NumberColumn, Matrix, List) :-
+    NewNumberRow is NumberRow - 1,
+    get_column(NewNumberRow, NumberColumn, Matrix, NewList),
+    nth1(NumberRow, Matrix, Row),
+    element(NumberColumn, Row, Elem),
+    append(NewList, [Elem], List).    
 
 buildRow(1, List, Result) :- 
     append(List, [Variable], Result).
@@ -116,18 +181,6 @@ buildList(NumberColumn, NumberRow, List, Result) :-
     NewNumber is NumberRow - 1,
     buildList(NumberColumn, NewNumber, List, NewList),
     append(NewList, [RowResult], Result).
-
-
-
-
-
-
-
-oldBuildRow(1, List, Result) :- append(List, [0], Result).
-oldBuildRow(Number, List, Result) :-
-    NewNumber is Number - 1,
-    buildRow(NewNumber, List, NewList),
-    append(NewList, [0], Result), write(Result).
 
 
 
